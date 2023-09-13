26d241bcca1693ae73841016ae45e3403eb5b0b7
n <>
2023-09-14 02:33:49 +1000

    made it so you can use the pause/resume button in Level 1, also started working on ensuring the settings sliders affect volume

--- Emotional Pizzeria/Assets/Scripts/KeepBGMusicPlaying.cs
+++ Emotional Pizzeria/Assets/Scripts/KeepBGMusicPlaying.cs
@@ -3,28 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class script : MonoBehaviour
+
+/// <summary>
+/// Script to ensure the background music persists across scenes
+/// Author: Rimon; last date modified - 14 September 2023
+/// </summary>
+public class KeepBGMusicPlaying : MonoBehaviour
 {
     // Credit to the following Unity tutorial:
     // https://www.youtube.com/watch?v=I_ZbPiI5p88
     // Start is called before the first frame update
     static bool audioHasStarted = false;
+
+/// <summary>
+/// Author - Rimon; last date modified - 14 September 2023
+/// 
+/// Just initializes our script by checking if there's any BGM already playing-- if that's the case
+/// we let the music play and have it persist, otherwise we stop it from running.
+/// </summary>
     void Start()
     {
         GameObject[] obj = GameObject.FindGameObjectsWithTag("bgm");
+        /* At any given moment, we should have only one background music track playing-- if there's already
+         * BGM audio playing then we should delete this audio track. */
         if (obj.Length > 1)
         {
             Destroy(this.gameObject);
         }
         else
         {
+            /* Keep the gameobject for audio persisting between scenes */
             DontDestroyOnLoad(this.gameObject);
         }
     }
 
-    // Update is called once per frame
+/// <summary>
+/// author - Rimon; last date modified - 14 September 2023
+/// Just runs a check every frame to see if we've moved into the Level One scene-- if we do then we should stop playing
+/// the audio.
+/// </summary>
     void Update()
     {
+        /* We only stop the music from playing once we enter level 1 */
         if (SceneManager.GetActiveScene().name == "Level 1")
         {
             Destroy(this.gameObject);
