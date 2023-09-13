2b8a445c5a87cf189531948a4b1f7b6e43930fde
sammi <txl.sammi@gmail.com>
2023-09-14 04:32:20 +1000

    created scripts for quiz management

---
+++ Emotional Pizzeria/Assets/Scripts/QuizManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    public List<QuestionAndAnswers> QnA;
+    public GameObject[] options;
+    public int currentQuestion;
+
+    public Text QuestionTxt;
+
+    private void Start() 
+    {
+        generateQuestion();
+    }
+
+    public void correct()
+    {
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
+    void setAnswers()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<Answers>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<Text>().text = QnA[currentQuestion].Answers[i];
+
+            if(QnA[currentQuestion].CorrectAnswer == i+1)
+            {
+                options[i].GetComponent<Answers>().isCorrect = true;
+            }
+        }
+    }
+
+    void generateQuestion()
+    {
+        currentQuestion = Random.Range(0, QnA.Count);
+        QuestionTxt.text = QnA[currentQuestion].Question;
+        setAnswers();
+    }
+}
