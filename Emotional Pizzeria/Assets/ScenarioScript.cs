8ceede7785ef8021cefc8ccdd905e205999b51f8
baejb1229059 <baejb@student.unimelb.edu.au>
2023-09-14 15:09:15 +1000

    fix some errors of priority

--- Emotional Pizzeria/Assets/ScenarioScript.cs
+++ Emotional Pizzeria/Assets/ScenarioScript.cs
@@ -7,12 +7,18 @@ public class ScenarioScript : MonoBehaviour
 {
     public string[] dir; 
     public string curDir; 
+    [SerializeField] private GameObject TextObject;
 
     // Start is called before the first frame update
     void Start()
     {
         dir = Directory.GetDirectories("Assets/Resources/Scenarios");
         curDir = dir[Random.Range(0, dir.Length)];
+        
+
+        TextScript sc = TextObject.GetComponent<TextScript>();
+        sc.SetText(curDir);
+        //Debug.Log(curDir);
         //Debug.Log("Hello" + curDir);
     }
 }
--- Emotional Pizzeria/Assets/TextScript.cs
+++ Emotional Pizzeria/Assets/TextScript.cs
@@ -13,27 +13,38 @@ public class TextScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        /*
         //GetComponent<SpriteRenderer>().sprite = expression_sprites[UnityEngine.Random.Range(0, expression_sprites.Length)];
         ScenarioScript sc = customer.GetComponent<ScenarioScript>();
         string scDir = sc.curDir;
+        Debug.Log(sc.curDir);
 
         //Debug.Log(Directory.GetFiles(Path.Combine(scDir, "sprite")));
 
         string[] files = Directory.GetFiles(Path.Combine(scDir, "situation"));
         string[] filtered = Array.FindAll(files, files => 
                                             !files.EndsWith("meta"));
+
         string sit_chosen = filtered[UnityEngine.Random.Range(0, filtered.Length)];
-        
+       
         StreamReader reader = new StreamReader(sit_chosen);
         string situation = reader.ReadToEnd();
         tmp.text = situation;
-        Debug.Log(situation);
+        //Debug.Log(situation);
+        */
    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SetText(string path) {
+        Debug.Log(path);
+
+        string[] files = Directory.GetFiles(Path.Combine(path, "situation"));
+        string[] filtered = Array.FindAll(files, files => 
+                                            !files.EndsWith("meta"));
+
+        string sit_chosen = filtered[UnityEngine.Random.Range(0, filtered.Length)];
+        StreamReader reader = new StreamReader(sit_chosen);
+        string situation = reader.ReadToEnd();
+        tmp.text = situation;
     }
 }
